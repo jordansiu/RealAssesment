dab5b42f69d80c45e84b629bcffd8a8565a8b4df
jordansiu <jordan.siu@my.bdsc.school.nz>
2025-05-23 12:03:26 +1200

    cart balance and discount

--- code.py
+++ code.py
@@ -3,3 +3,17 @@ products = {
     "banana":{"price": 1.5, "discount": False},
     "pineapple":{"price":3.0,"discount": True}
 }
+balance = 20.0
+cart = {}
+
+def show_products():
+    print("\nAvailable Products:")
+    for item, details in products.items():
+        discount_msg = "Yes" if details["discount"] else "No"
+        print(f"{item.title()} - ${details['price']} | Discount: {discount_msg}")
+
+def show_cart():
+    print("\nYour Cart:")
+    total = 0
+    for item, qty in cart.items():
+        price = products[item]["price"]
