26a21fdf93a902613f4fa5a248054de23e7edc8f
jordansiu <jordan.siu@my.bdsc.school.nz>
2025-05-26 09:20:21 +1200

    menu loop

--- code.py
+++ code.py
@@ -1,19 +1,11 @@
 products = {
     "apple": {"price": 1.0, "discount": True},
     "banana":{"price": 1.5, "discount": False},
-    "pineapple":{"price":3.0,"discount": True}
+    "pineapple":{"price":3.0,"discount": True},
 }
 balance = 20.0
 cart = {}
 
-def show_products():
-    print("\nAvailable Products:")
-    for item, details in products.items():
-        discount_msg = "Yes" if details["discount"] else "No"
-        print(f"{item.title()} - ${details['price']} | Discount: {discount_msg}")
-
-def show_cart():
-    print("\nYour Cart:")
-    total = 0
-    for item, qty in cart.items():
-        price = products[item]["price"]
+while True:
+    print("\n--- Menu ---")
+    item_name = input("Enter item name (or type 'total' to finish): ").lower()
