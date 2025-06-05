5e78bfa8948ee8823dba665f06a4e8b2e7d43143
Jordan Siu <jordan.siu@my.bdsc.school.nz>
2025-06-06 11:53:17 +1200

    new products

---
+++ shoppingsimulator.py
@@ -0,0 +1,7 @@
+products = {
+    "apple": {"price": 2.0, "discount": True},
+    "banana": {"price": 1.5, "discount": False},
+    "milk": {"price": 3.0, "discount": True},
+    "bread": {"price": 2.5, "discount": False},
+    "cheese": {"price": 4.0, "discount": True}
+}
