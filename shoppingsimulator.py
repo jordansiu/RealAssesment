62d895698458f36688389f9a63c9986326d93820
Jordan Siu <jordan.siu@my.bdsc.school.nz>
2025-06-09 12:46:16 +1200

    displays current cart

--- shoppingsimulator.py
+++ shoppingsimulator.py
@@ -15,3 +15,16 @@ def display_products():
         discount = "Yes" if info["discount"] else "No"
         print(f"- {item.title()} | Price: ${info['price']:.2f} | Discount: {discount}")
     print("Type 'add', 'remove', or 'total' when you're ready.\n")
+
+def display_cart():
+    print("\n CURRENT CART:")
+    if not cart:
+        print("Cart is empty.")
+    else:
+        for item, qty in cart.items():
+            price = products[item]["price"]
+            print(f"- {item.title()} x{qty} = ${price * qty:.2f}")
+        print(f"Subtotal so far: ${calculate_total():.2f}")
+
+def calculate_total():
+    return sum(products[item]["price"] * qty for item, qty in cart.items())
